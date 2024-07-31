cce2c76600255ab0b5ebd43811e510485d1a3c85
akaz-cyber <ajrisonic62@gmail.com>
2024-07-31 16:15:32 +0700

    menambahkan fitur terbaru

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,6 @@ dependencies {
     implementation("com.google.firebase:firebase-storage")
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.android.gms:play-services-auth")
+    implementation("com.google.android.gms:play-services-auth:20.5.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
 }
